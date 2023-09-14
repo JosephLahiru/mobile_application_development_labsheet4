dda8608fde1118bbc19f3e67bc23bf0bcb5809ee
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-14 13:48:23 +0530

    Initial commit

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet4/MainActivity.java
@@ -0,0 +1,14 @@
+package me.mtron.mobile_application_development_labsheet4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
