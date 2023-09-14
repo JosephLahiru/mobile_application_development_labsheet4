adf20938fda770a17026cf9f48f8fefac3ec954c
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-14 14:11:01 +0530

    started Activity2.java

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet4/example_02/Activity2.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet4.example_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet4.R;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
